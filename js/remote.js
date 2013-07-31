650b3cf40a9a3e589cf618fb5dfb50695ae1e523
uberalex <oconnoat@gmail.com>
2013-07-31 16:45:53 +0100

    need to figure out how to get the note text into the message payload

--- js/remote.js
+++ js/remote.js
@@ -1,11 +1,12 @@
 function openWindow(url){
-    var popup = window.open(url);
+    var popup = window.open(url, 'width=400,height=200');
     return popup;
 }
 function message(targetWindow, message, origin){
-    alert('message sent');
     targetWindow.postMessage(message, origin);
+
 }
 function receivemessage(event){
+    document.getElementById('notes').innerHTML = 'event';
     console.log(event);
 }
